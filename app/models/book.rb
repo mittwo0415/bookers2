5a7ca3467c97545ba4b79a9a6f8ad10ffcce9e62
EC2 Default User <ec2-user@ip-172-31-6-103.ap-northeast-1.compute.internal>
2024-09-30 11:22:39 +0000

    通知機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,12 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :body, length: { maximum: 200 }
+  
+  has_many :notifications, as: :notifiable, dependent: :destroy
+  
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  has_many :notifications, dependent: :destroy
+  
    has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
